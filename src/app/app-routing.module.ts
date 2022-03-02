73c19dfccbf7ece10905cec7fed29499cc7f58e8
Mahbobeh <mahbub.khazaee@gmail.com>
2022-03-02 03:57:31 +0330

    add 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,20 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {NotFoundComponent} from "./core/not-found/not-found.component";
 
 const routes: Routes = [
   {
-    path: 'login',
-    loadChildren: () =>
-      import('./login/login.module').then((mod) => mod.LoginModule),
-    data: {applyPreload: true},
-  },
-  {path: '', redirectTo: 'login', pathMatch: 'full'}];
+  path: 'login',
+  loadChildren: () =>
+    import('./login/login.module').then((mod) => mod.LoginModule),
+  data: { applyPreload: true },
+},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404' },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }

f8fd2fe84c77a2a7e21fd35c328968bb6f6242da
Mahbobeh <mahbub.khazaee@gmail.com>
2022-03-06 05:46:27 +0330

    make tasks editable

--- src/app/task/edit-task/edit-task.component.ts
+++ src/app/task/edit-task/edit-task.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {BaseComponent} from "../../core/components/base/base.component";
 
 @Component({
   selector: 'app-edit-task',
   templateUrl: './edit-task.component.html',
   styleUrls: ['./edit-task.component.scss']
 })
-export class EditTaskComponent implements OnInit {
+export class EditTaskComponent extends BaseComponent implements OnInit {
+  task = this.data.task;
 
-  constructor() { }
+  constructor(
+    private dialogRef: MatDialogRef<EditTaskComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+    super();
+  }
 
   ngOnInit(): void {
   }
 
+  onCancel(): void {
+    this.dialogRef.close();
+  }
+
+  onCaptionValueChange(ev: any) {
+    this.task.caption = ev.target.value;
+  }
 }
